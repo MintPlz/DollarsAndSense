296fdebf88cf81e7684b7159d3d41ee4932164ad
MintPlz <73250650+MintPlz@users.noreply.github.com>
2023-06-14 16:04:21 -0500

    Beginning test for cross platform smooth scrolling

--- scrollManager.js
+++ scrollManager.js
@@ -1,5 +1,21 @@
-//Onscroll event listener
-//window.onscroll = function() {console.log("scrolling")};
+//Get sections and buttons
+const sections = document.querySelectorAll("section");
+const btns = document.querySelectorAll(".navbar a");
 
-//Save navbar for later use
-//var navbar = document.getElementsByClassName("navbar");
+//testing
+function test() {
+
+  console.log("testing");
+
+  console.log(sections)
+
+  sections.forEach(section => {
+    console.log(section.id)
+  });
+
+  console.log(btns)
+
+  btns.forEach(btn => {
+    console.log(btn.textContent)
+  });
+}
